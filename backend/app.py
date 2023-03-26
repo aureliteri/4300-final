db5c0351821c448bd043499c8fca75601a594c59
Amy Ouyang <amyouyang7@gmail.com>
2023-03-26 17:33:32 -0400

    enable load file

--- backend/app.py
+++ backend/app.py
@@ -15,13 +15,13 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = ""
-MYSQL_PORT = 3306
-MYSQL_DATABASE = "atlas_obscura_testing_data"
+MYSQL_PORT = 4534
+MYSQL_DATABASE = "atlastable"
 
 mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
-# mysql_engine.load_file_into_db()
+mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
 CORS(app)
