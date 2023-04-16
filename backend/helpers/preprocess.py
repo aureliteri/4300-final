22c2b73f37d950c18b8c42e5e7363ed1cfc8b56a
Amy Ouyang <amyouyang7@gmail.com>
2023-04-16 18:17:18 -0400

    final dataset complete

--- backend/helpers/preprocess.py
+++ backend/helpers/preprocess.py
@@ -4,21 +4,24 @@ from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
 
-with open('/Users/aaronkang/Desktop/current_semester/SP23/CS4300/4300-final/atlas-obscura-scraper/atlas-obscura-full.json', 'r') as infile:
+with open('/Users/amyouyang/Documents/Cornell/Senior/CS4300/4300-final/atlas-obscura-scraper/pages-run/merged-pages.json', 'r') as infile:
     input_array = json.load(infile)
 
 arr = []
 
+count = 0
 for dictionary in input_array:
-
+    dictionary['index'] = count
+    count += 1
     description_value = dictionary['description']
-    description_value = description_value.translate(str.maketrans('', '', string.punctuation))
+    description_value = description_value.translate(
+        str.maketrans('', '', string.punctuation))
     words = description_value.split()
-    lemmatized_description_value = ' '.join([lemmatizer.lemmatize(word) for word in words])
+    lemmatized_description_value = ' '.join(
+        [lemmatizer.lemmatize(word) for word in words])
 
     dictionary['lemmatized_description'] = lemmatized_description_value
     arr.append(dictionary)
 
-with open('/Users/aaronkang/Desktop/current_semester/SP23/CS4300/4300-final/atlas-obscura-scraper/atlas-obscura-full-new.json', 'w') as outfile:
+with open('/Users/amyouyang/Documents/Cornell/Senior/CS4300/4300-final/atlas-obscura-scraper/atlas-obscura-pages-full.json', 'w') as outfile:
     json.dump(arr, outfile)
-
