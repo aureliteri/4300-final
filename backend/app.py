468d558a0d5f779e0f0b626224518ef7aa555f3d
Amy Ouyang <amyouyang7@gmail.com>
2023-04-17 11:43:33 -0400

    merging old-state to master

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,9 @@ from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 from helpers.PartOne import *
 from helpers.PartTwo import *
 
-# ROOT_PATH for linking with all your files. 
+# ROOT_PATH for linking with all your files.
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
@@ -16,18 +16,19 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 3306
-MYSQL_DATABASE = "atlas_obscura_testing_data"
+MYSQL_DATABASE = ""
 
-mysql_engine = MySQLDatabaseHandler(MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE)
+mysql_engine = MySQLDatabaseHandler(
+    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
-#mysql_engine.load_file_into_db()
+# mysql_engine.load_file_into_db()
 
 app = Flask(__name__)
 CORS(app)
 
-# Sample search, the LIKE operator in this case is hard-coded, 
-# but if you decide to use SQLAlchemy ORM framework, 
+# Sample search, the LIKE operator in this case is hard-coded,
+# but if you decide to use SQLAlchemy ORM framework,
 # there's a much better and cleaner way to do this
 # def sql_search(episode):
 #     query_sql = f"""SELECT * FROM episodes WHERE LOWER( title ) LIKE '%%{episode.lower()}%%' limit 10"""
@@ -35,24 +36,29 @@ CORS(app)
 #     data = mysql_engine.query_selector(query_sql)
 #     return json.dumps([dict(zip(keys,i)) for i in data])
 
+
 def sql_search(table_name):
     query_sql = f"""SELECT * FROM {table_name}"""
-    #keys = ["index","attraction","location","blurb","url","description"]
-    keys = ["index", "description", "lemmatized_description", "url", "attraction", "location", "blurb"]
+    keys = ["index", "attraction", "location", "blurb",
+            "url", "description", "lemmatized_description"]
+    # keys = ["index", "description", "lemmatized_description",
+    #         "url", "attraction", "location", "blurb"]
     data = mysql_engine.query_selector(query_sql)
-    return [dict(zip(keys,i)) for i in data]
+    return [dict(zip(keys, i)) for i in data]
+
 
 data = sql_search('atlasnew')
 partOne = PartOne(data, 5000)
 partTwo = PartTwo(partOne._tfidf_vec,
-                    partOne._array_with_country, 
-                    partOne._attraction_by_token, 
-                    partOne._index_to_vocab)
+                  partOne._array_with_country,
+                  partOne._attraction_by_token,
+                  partOne._index_to_vocab)
+
 
 def generate_tags(countries):
     tag_dict = partOne.generate_tags([countries])
     print(tag_dict, type(tag_dict))
-    
+
     return tag_dict
 
 
@@ -61,27 +67,30 @@ def country_list():
     print("form submitted")
     country_set = set()
     for entry in partOne._array_with_country:
-      country_set.add(entry["country"])
+        country_set.add(entry["country"])
     print(country_set)
     return list(country_set)
+
 
 @app.route("/countries")
 def get_countries():
     countries = request.args.get("countries")
-    tags_dict= generate_tags(countries)
+    tags_dict = generate_tags(countries)
     tags = tags_dict[countries]
     print(tags)
     return tags
 
+
 @app.route("/output")
 def generate_output():
     tags = request.args.get("tags")
-    tags= tags.strip().split(",")
+    tags = tags.strip().split(",")
     output_tuple = partTwo.get_top_attractions(partTwo.pmi, tags)
     output = [location for score, location in output_tuple]
     print(output)
 
     return output
+
 
 @app.route("/")
 def home():
@@ -92,13 +101,14 @@ def home():
     # print(tag_dict)
 
     # partTwo = PartTwo(partOne._tfidf_vec,
-    #                   partOne._array_with_country, 
-    #                   partOne._attraction_by_token, 
+    #                   partOne._array_with_country,
+    #                   partOne._attraction_by_token,
     #                   partOne._index_to_vocab)
     # print(partTwo.find_most_similar_words(partTwo.pmi, "castle"))
     # weighted_tags = ["castle", "castles", "hungary", "romania", "von"]
     # # print(tag_dict['ranked_words'])
     # print(partTwo.get_top_attractions(partTwo.pmi, weighted_tags))
-    return render_template('base.html',title="home")
+    return render_template('base.html', title="home")
+
 
 app.run(debug=True)
--- backend/helpers/PartTwo.py
+++ backend/helpers/PartTwo.py
@@ -3,67 +3,56 @@ import numpy as np
 
 TOPK = 10
 
+
 class PartTwo:
-  def __init__(self, array_with_country, index_to_vocab):
-    self._array_with_country = array_with_country
-    self._count_vec = self.build_count_vectorization(5000, "english")
-    self._token_counts = self.generate_tf(self._count_vec)
-    self._inverted_index = self.generate_inverted_index(self._token_counts)
-    self._index_to_vocab = index_to_vocab
-    self.pmi = self.generate_pmi_mat()
-  
+    def __init__(self, tfidf_vec, array_with_country, attraction_by_token, index_to_vocab):
+        self._tfidf_vec = tfidf_vec
+        self._array_with_country = array_with_country
+        self._attraction_by_token = attraction_by_token
+        self._count_vec = self.build_count_vectorization(5000, "english")
+        self._token_counts = self.generate_tf(self._count_vec)
+        self._index_to_vocab = index_to_vocab
+        self.pmi = self.generate_pmi_mat()
 
-  def build_count_vectorization(self, max_features, stop_words, max_df=0.8, min_df= 10):
-     return CountVectorizer(max_features = max_features, 
-                           stop_words = stop_words, 
-                           max_df = max_df, 
-                           min_df = min_df)
-  
-  def generate_tf(self, count_vec):
-    return count_vec.fit_transform([d["lemmatized_description"].lower() for d in self._array_with_country]).toarray()
-  
-  def generate_inverted_index(self, token_counts):
-    inverted_index = {}
-    for token_index in range(token_counts.shape[1]):
-        token = self._count_vec.get_feature_names_out()[token_index]
-        token_tuples = [(doc_index, token_counts[doc_index, token_index]) for doc_index in range(token_counts.shape[0]) if token_counts[doc_index, token_index] > 0]
-        inverted_index[token] = token_tuples
-    
-    print(inverted_index)
-    return inverted_index
+    def build_count_vectorization(self, max_features, stop_words, max_df=0.8, min_df=10):
+        return CountVectorizer(max_features=max_features,
+                               stop_words=stop_words,
+                               max_df=max_df,
+                               min_df=min_df)
 
-  def generate_pmi_mat(self):
-    df = np.sum(self._token_counts.T,1)
-    cooccurance_mat = np.dot(self._token_counts.T, self._token_counts)
-    pmi_part = cooccurance_mat/df
-    return pmi_part/df.reshape(df.shape[0],1)
-  
-  
-  def find_most_similar_words(self, sim_mat, word, topk = TOPK):
-    features = self._count_vec.get_feature_names_out()
-    if word not in features:
-        print(word, 'is OOV.')
-        return None 
-    idx = features.index(word)
-    sorted_words_ind = np.argsort(sim_mat[idx])[::-1]
-    output = [tuple((features[ind],sim_mat[idx][ind])) for ind in sorted_words_ind][:topk+1]
-    return output
-  
-  def get_top_attractions(self,sim_mat, weighted_tags):
-    scores = []
-    for destination in self._array_with_country:
-      description = destination["description"].lower()
-      split_description = set(description.split())
-      score = 0
-      for tag in weighted_tags:
-        most_similar_words = set([x[0] for x in self.find_most_similar_words(sim_mat, tag)])
-        print("intersection")
-        score += len(list(split_description.intersection(most_similar_words)))
-      scores.append(tuple((score, destination["attraction"])))
-    output = sorted(scores,key=lambda x: x[0], reverse=True)[:10]
-    print("Destinations to visit:")
-    return output
+    def generate_tf(self, count_vec):
+        return count_vec.fit_transform([d["description"].lower() for d in self._array_with_country]).toarray()
 
+    def generate_pmi_mat(self):
+        df = np.sum(self._attraction_by_token.T, 1)
+        cooccurance_mat = np.dot(
+            self._attraction_by_token.T, self._attraction_by_token)
+        pmi_part = cooccurance_mat/df
+        return pmi_part/df.reshape(df.shape[0], 1)
 
+    def find_most_similar_words(self, sim_mat, word, topk=TOPK):
+        features = self._tfidf_vec.get_feature_names()
+        if word not in features:
+            print(word, 'is OOV.')
+            return None
+        idx = features.index(word)
+        sorted_words_ind = np.argsort(sim_mat[idx])[::-1]
+        output = [tuple((features[ind], sim_mat[idx][ind]))
+                  for ind in sorted_words_ind][:topk+1]
+        return output
 
-  
+    def get_top_attractions(self, sim_mat, weighted_tags):
+        scores = []
+        for destination in self._array_with_country:
+            description = destination["description"].lower()
+            split_description = set(description.split())
+            score = 0
+            for tag in weighted_tags:
+                most_similar_words = set(
+                    [x[0] for x in self.find_most_similar_words(sim_mat, tag)])
+                print("intersection")
+                score += len(list(split_description.intersection(most_similar_words)))
+            scores.append(tuple((score, destination["attraction"])))
+        output = sorted(scores, key=lambda x: x[0], reverse=True)[:10]
+        print("Destinations to visit:")
+        return output
