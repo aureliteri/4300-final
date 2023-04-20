793d4756a7aee2ffd3664de8987b09998309bd5e
Shefali Kumar <sk2432@cornell.edu>
2023-04-20 15:54:28 -0400

    typo at MYSQL_DATABASE (I am just a person.)

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
 MYSQL_USER = "root"
 MYSQL_USER_PASSWORD = ""
 MYSQL_PORT = 4534
-MYSQL_DATABASE = "travellocomation_db"
+MYSQL_DATABASE = "travellocomotion_db"
 
 mysql_engine = MySQLDatabaseHandler(
     MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
